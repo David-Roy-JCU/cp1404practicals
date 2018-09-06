18788dada6d11d0a9e1f4d79711ab74c1cdfd54b
David-Roy-JCU <david.roy@my.jcu.edu.au>
2018-09-06 13:52:42 +1000

    add car.py class and used_cars.py file

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,9 @@
+"""
+CP1404 prac 05 hec colours dictionary
+"""
+
+COLOURS = {"azure1": "#f0ffff", "coral": "#ff7f50", "darkorchid": "#9932cc"}
+
+colour = input("Enter colour name").lower()
+valid_colour = False
+while valid_colour != True:
